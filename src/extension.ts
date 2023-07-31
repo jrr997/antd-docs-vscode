941a666b399a2b2f304b87c5de58fccbb69ad34c
jrr997 <zackchangjx@foxmail.com>
2023-07-31 14:56:47 +0800

    feat: add token config

--- src/extension.ts
+++ src/extension.ts
@@ -7,16 +7,31 @@ import { validateVersion } from './utils';
 
 export async function activate(context: vscode.ExtensionContext) {
   const workspaceState = context.workspaceState;
-  const config = vscode.workspace.getConfiguration('AntdDoc');
+  const config = vscode.workspace.getConfiguration('AntdDocs');
   const versionInWorkspace = config.get('docVersion') as string | undefined;
   const languageInWorkspace = config.get('language') as string | undefined;
 
+  const githubToken = config.get('githubToken') as string;
+  if (!githubToken) {
+    vscode.window.showInformationMessage('Please set your github token in AntdDocs.githubToken');
+    return;
+  }
 
-  const fetchAndParseDocs = async (version: string) => {
+  // Fetch docs when github token exists and version is valid
+  const shouldFetch = () => {
+    const githubToken = config.get('githubToken') as string;
+    if (!githubToken) { return false; }
+    const documentData = workspaceState.get('documentData') as ParsedDocsState;
+    if (!documentData?.parsedDocs || !Object.keys(documentData.parsedDocs).length) { return false; }
+    const { version } = documentData;
+    return validateVersion(versionInWorkspace) && version !== versionInWorkspace;
+  };
+
+  const fetchAndParseDocs = async (version: string, token: string) => {
     console.log('Fetching Docs!');
     console.log('docs version: ' + version);
 
-    const docsMap = await fetchDoc(version);
+    const docsMap = await fetchDoc(version, token);
     if (docsMap) {
       console.log('fetch Doc successfully!');
       const parsedDocs = parseDoc(docsMap, version);
@@ -26,12 +41,12 @@ export async function activate(context: vscode.ExtensionContext) {
   console.log('versionInWorkspace: ', versionInWorkspace);
 
   if (versionInWorkspace) {
-    workspaceState.update('documentData',  undefined); // For debugging docs fetching and parsing, please do not remove it.
+    workspaceState.update('documentData', undefined); // For debugging docs fetching and parsing, please do not remove it.
     const documentData = workspaceState.get('documentData') as ParsedDocsState;
     if (documentData && versionInWorkspace === documentData.version) {
       console.log('docsMap existed!', documentData);
     } else if (validateVersion(versionInWorkspace)) {
-      fetchAndParseDocs(versionInWorkspace);
+      fetchAndParseDocs(versionInWorkspace, githubToken);
     }
   }
 
@@ -43,27 +58,35 @@ export async function activate(context: vscode.ExtensionContext) {
     if (validateVersion(inputVersion)) {
       config.update('docVersion', inputVersion);
       vscode.window.showInformationMessage(`Version changed! Start to update docs v${inputVersion}`);
-      fetchAndParseDocs(inputVersion!);
+      // TODO: 闭包可能导致githubToken为空
+      fetchAndParseDocs(inputVersion!, githubToken);
     } else {
       vscode.window.showInformationMessage(`Failed! Version ${inputVersion} is invalid.`);
     }
   });
 
-  // let listener = vscode.workspace.onDidChangeConfiguration(event => {
-  //   if (event.affectsConfiguration('AntdDoc.docVersion')) {
-  //     const config = vscode.workspace.getConfiguration('AntdDoc');
-  //     const newVersion = config.get('docVersion') as string;
-  //     if (validateVersion(newVersion)) {
-  //       config.update('docVersion', newVersion);
-  //       vscode.window.showInformationMessage(`Version changed! Antd Docs version is "${newVersion}"`);
-  //       fetchAndParseDocs(newVersion);
-  //     }
-  //   }
-  // });
+  let listener = vscode.workspace.onDidChangeConfiguration(event => {
+    // if (event.affectsConfiguration('AntdDoc.docVersion')) {
+    //   const config = vscode.workspace.getConfiguration('AntdDoc');
+    //   const newVersion = config.get('docVersion') as string;
+    //   if (validateVersion(newVersion)) {
+    //     config.update('docVersion', newVersion);
+    //     vscode.window.showInformationMessage(`Version changed! Antd Docs version is "${newVersion}"`);
+    //     fetchAndParseDocs(newVersion);
+    //   }
+    // }
+    if (event.affectsConfiguration('AntdDocs.githubToken')) {
+      const config = vscode.workspace.getConfiguration('AntdDocs');
+      const newToken = config.get('githubToken') as string;
+      if (shouldFetch()) {
+        fetchAndParseDocs(versionInWorkspace!, newToken);
+      }
+    }
+  });
 
   const provider = vscode.languages.registerHoverProvider(['typescript', 'typescriptreact', 'javascript'], new DocsHoverProvider(context, languageInWorkspace));
 
-  context.subscriptions.push(setVersion, provider);
+  context.subscriptions.push(setVersion, provider, listener);
 }
 
 
--- src/fetchDocs.ts
+++ src/fetchDocs.ts
@@ -2,20 +2,20 @@ import { Octokit } from '@octokit/rest';
 import { ANTD_GITHUB, GITHUB_TOKEN, excludeDirs } from './constant';
 import { DocsLang, DocsMap } from './types';
 
-const octokit = new Octokit({
-  auth: GITHUB_TOKEN
-});
+// const octokit = new Octokit({
+//   auth: GITHUB_TOKEN
+// });
 
-const getAntdContent = (path: string, ref?: string) => octokit.rest.repos.getContent({
+const getAntdContent = (path: string, token: string, ref?: string) => new Octokit({ auth: token }).rest.repos.getContent({
   owner: ANTD_GITHUB.OWNER,
   repo: ANTD_GITHUB.REPO,
   path,
   ref
 });
 
-export const getFileContent = async (owner: string, repo: string, path: string) => {
+export const getFileContent = async (owner: string, repo: string, path: string, token: string) => {
   try {
-    const response = await octokit.rest.repos.getContent({
+    const response = await new Octokit({auth: token}).rest.repos.getContent({
       owner: owner,
       repo: repo,
       path: path,
@@ -26,9 +26,9 @@ export const getFileContent = async (owner: string, repo: string, path: string) 
   }
 };
 
-export const getComponentDirInfos = async () => {
+export const getComponentDirInfos = async (token: string) => {
   try {
-    const response = await getAntdContent('/components');
+    const response = await getAntdContent('/components', token);
     const { data } = response;
     if (Array.isArray(data)) {
       const componentDirInfos = data.filter(item => item.type === 'dir');
@@ -41,12 +41,12 @@ export const getComponentDirInfos = async () => {
 
 
 
-export const fetchDoc = async (ref?: string) => {
-  const dirInfos = await getComponentDirInfos();
+export const fetchDoc = async (ref: string, token: string) => {
+  const dirInfos = await getComponentDirInfos(token);
   const zhPromises = dirInfos
-    ?.map(dirInfo => getAntdContent(`${dirInfo.path}/${ANTD_GITHUB.ZH_DOC_NAME}`, ref));
+    ?.map(dirInfo => getAntdContent(`${dirInfo.path}/${ANTD_GITHUB.ZH_DOC_NAME}`,token, ref));
   const enPromises = dirInfos
-    ?.map(dirInfo => getAntdContent(`${dirInfo.path}/${ANTD_GITHUB.EN_DOC_NAME}`, ref));
+    ?.map(dirInfo => getAntdContent(`${dirInfo.path}/${ANTD_GITHUB.EN_DOC_NAME}`, token, ref));
   try {
     const res = await Promise.allSettled([...zhPromises!, ...enPromises!]);
     let docsMap: DocsMap = {};
@@ -74,37 +74,37 @@ export const fetchDoc = async (ref?: string) => {
   }
 };
 
-export const getComponentsWithDocsFiles = async () => {
-  const dirInfos = await getFileContent(ANTD_GITHUB.OWNER, ANTD_GITHUB.REPO, '/components');
-  if (Array.isArray(dirInfos)) {
-    const componentDirInfos = dirInfos.filter(item => item.type === 'dir');
-    // 检查是否有md文档
-    const promises = componentDirInfos.map((item) => {
-      return octokit.rest.repos.getContent({
-        owner: ANTD_GITHUB.OWNER,
-        repo: ANTD_GITHUB.REPO,
-        path: item.path,
-      });
-    });
-    let componentsWithoutDocsFiles: string[] = [];
-    try {
-      const res = await Promise.all(promises);
-      if (Array.isArray(res)) {
-        res.forEach(item => {
-          if (Array.isArray(item.data)) {
-            const fileNames = item.data.map(({ name }) => name);
-            if (!fileNames.includes(ANTD_GITHUB.EN_DOC_NAME) || !fileNames.includes(ANTD_GITHUB.ZH_DOC_NAME)) {
-              const componentName = item.data[0].path.split('/')[1];
-              componentsWithoutDocsFiles.push(componentName);
-            }
-          }
-        });
-      }
-    } catch (e) {
-      console.log(e);
-    }
-    console.log(componentsWithoutDocsFiles);
+// export const getComponentsWithDocsFiles = async (token: string) => {
+//   const dirInfos = await getFileContent(ANTD_GITHUB.OWNER, ANTD_GITHUB.REPO, '/components', token);
+//   if (Array.isArray(dirInfos)) {
+//     const componentDirInfos = dirInfos.filter(item => item.type === 'dir');
+//     // 检查是否有md文档
+//     const promises = componentDirInfos.map((item) => {
+//       return octokit.rest.repos.getContent({
+//         owner: ANTD_GITHUB.OWNER,
+//         repo: ANTD_GITHUB.REPO,
+//         path: item.path,
+//       });
+//     });
+//     let componentsWithoutDocsFiles: string[] = [];
+//     try {
+//       const res = await Promise.all(promises);
+//       if (Array.isArray(res)) {
+//         res.forEach(item => {
+//           if (Array.isArray(item.data)) {
+//             const fileNames = item.data.map(({ name }) => name);
+//             if (!fileNames.includes(ANTD_GITHUB.EN_DOC_NAME) || !fileNames.includes(ANTD_GITHUB.ZH_DOC_NAME)) {
+//               const componentName = item.data[0].path.split('/')[1];
+//               componentsWithoutDocsFiles.push(componentName);
+//             }
+//           }
+//         });
+//       }
+//     } catch (e) {
+//       console.log(e);
+//     }
+//     console.log(componentsWithoutDocsFiles);
 
-    return componentsWithoutDocsFiles;
-  }
-};
+//     return componentsWithoutDocsFiles;
+//   }
+// };
